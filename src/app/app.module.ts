5ea835f9537298ce28b2c5fd5bb2290e7753f079
kunwar1104 <harshvirsinh1104@gmail.com>
2023-12-07 15:10:02 +0530

    made header and footer fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
         
     ]
 })
